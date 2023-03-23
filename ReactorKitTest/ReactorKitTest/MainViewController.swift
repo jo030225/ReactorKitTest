452ac5c0f622909fa9fd46781d92216f3af1c530
jo030225 <whwngur123@naver.com>
2023-03-23 23:00:15 +0900

    :art: rename VC -> MainVC

--- ReactorKitTest/ReactorKitTest/MainViewController.swift
+++ ReactorKitTest/ReactorKitTest/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  ReactorKitTest
 //
 //  Created by 조주혁 on 2023/03/23.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
